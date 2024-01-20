2fd63175600a2f3423eaef301b0c525d4c0f221a
ankit3146 <an225884@dal.ca>
2024-01-19 19:01:32 -0500

    Added responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,13 @@ const Header = () => {
       <Grid container spacing={2} sx = {{  backgroundColor: '#E9E6E2', paddingTop: '2%'}}>
         <Grid item xs={1} />
         <Grid item xs={1} />
-        <Grid item xs={4}>
+        <Grid item xs={3} sx={{ display: 'flex', justifyContent: 'start'}}>
           <Box sx={{ display: 'flex' }}>
             <img src="../../assets/logo.svg" alt="Sapt Logo" />
           </Box>
         </Grid>
+
+        <Grid item xs={1} />
         <Grid item xs={5}>
           <List sx={{
             display: 'flex',
@@ -22,13 +24,13 @@ const Header = () => {
             padding: '2% 5%'
           }}>
             <Link href="/" underline="none">
-              <Typography sx={{ color: '#FFFFFF', fontSize: '20px', fontFamily: 'Jost' }}>Home</Typography>
+              <Typography sx={{ color: '#FFFFFF', fontSize: { sm: '1.0rem', md: '1.0rem', lg: '1.2rem' }, fontFamily: 'Jost' }}>Home</Typography>
             </Link>
             <Link href="/about" underline="none">
-              <Typography sx={{ color: '#FFFFFF', fontSize: '20px', fontFamily: 'Jost' }}>About</Typography>
+              <Typography sx={{ color: '#FFFFFF', fontSize: { sm: '1.0rem', md: '1.0rem', lg: '1.2rem' }, fontFamily: 'Jost' }}>About</Typography>
             </Link>
-            <Typography sx={{ color: '#FFFFFF', fontSize: '20px', fontFamily: 'Jost' }}>Services</Typography>
-            <Typography sx={{ color: '#FFFFFF', fontSize: '20px', fontFamily: 'Jost' }}>Contact Us</Typography>
+            <Typography sx={{ color: '#FFFFFF', fontSize: { sm: '1.0rem', md: '1.0rem', lg: '1.2rem' }, fontFamily: 'Jost' }}>Services</Typography>
+            <Typography sx={{ color: '#FFFFFF', fontSize: { sm: '1.0rem', md: '1.0rem', lg: '1.2rem' }, fontFamily: 'Jost' }}>Contact Us</Typography>
           </List>
         </Grid>
         <Grid item xs={1} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,68 +26,71 @@ const Home = () => {
     <>
       <div style={{ position: 'relative' }}>
         <Box height={150} style={{ backgroundColor: '#E9E6E2' }} />
-        <Grid container sx={{ display: 'flex', flexDirection: 'row', backgroundColor: '#E9E6E2' }}>
-          <Grid item xs={1} />
-          <Grid item xs={1} />
-          <Grid item xs={3} sx={{ display: 'flex', flexDirection: 'row' }}>
-            <Typography sx={{ color: '#1B1B1B', fontSize: '7em', fontFamily: 'RammettoOne', lineHeight: '1em' }}> WE </Typography>
-            <Container sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', marginLeft: '12%' }}>
-              <Typography sx={{ color: '#4D69FF', fontSize: '18px', fontFamily: 'Jost-600' }}> Design </Typography>
-              <Typography sx={{ color: '#4D69FF', fontSize: '18px', fontFamily: 'Jost-600' }}> Develop </Typography>
-              <Typography sx={{ color: '#4D69FF', fontSize: '18px', fontFamily: 'Jost-600' }}> Code </Typography>
-            </Container>
+        <Grid container spacing={2} direction="row" sx={{ backgroundColor: '#E9E6E2' }}>
+          <Grid item md={1} />
+          <Grid item md={1} />
+          <Grid item lg = {3} md={2} sx={{ display: 'flex', justifyContent: 'center'}}>
+            <Typography sx={{ color: '#1B1B1B', fontSize: { sm: '6rem', md: '5rem', lg: '7rem' }, fontFamily: 'RammettoOne', lineHeight: '1em' }}> WE </Typography>
           </Grid>
-          <Grid item xs={5} sx={{ marginLeft: '5%' }}>
-            <Typography sx={{ color: '#1B1B1B', fontSize: '1.5em', fontFamily: 'RammettoOne', width: '100%', textAlignLast: 'justify' }}>
-              For companies ranging from <br />
+          <Grid item md={1} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly'}}>
+            <Typography sx={{ color: '#4D69FF', fontSize: { sm: '1.2rem', md: '1.1rem', lg: '1.2rem' }, fontFamily: 'Jost-600' }}> Design </Typography>
+            <Typography sx={{ color: '#4D69FF', fontSize: { sm: '1.2rem', md: '1.1rem', lg: '1.2rem' }, fontFamily: 'Jost-600' }}> Develop </Typography>
+            <Typography sx={{ color: '#4D69FF', fontSize: { sm: '1.2rem', md: '1.1rem', lg: '1.2rem' }, fontFamily: 'Jost-600' }}> Code </Typography>
+          </Grid>
+          <Grid item md={5} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly' }}>
+            <Typography sx={{ color: '#1B1B1B', fontSize: { sm: '1.1em', md: '1.1em', lg: '1.4em' }, fontFamily: 'RammettoOne', textAlignLast: 'justify' }}>
+              For companies ranging from
             </Typography>
-            <Typography sx={{ color: '#1B1B1B', fontSize: '1.5em', fontFamily: 'RammettoOne', width: '100%', textAlignLast: 'justify' }}>
-              startups to the most iconic brands <br />
+            <Typography sx={{ color: '#1B1B1B', fontSize: { sm: '1.1em', md: '1.1em', lg: '1.4em' }, fontFamily: 'RammettoOne', textAlignLast: 'justify' }}>
+              startups to the most iconic brands
             </Typography>
-            <Typography sx={{ color: '#1B1B1B', fontSize: '1.5em', fontFamily: 'RammettoOne', width: '100%' }}>
-              in the world <Typography sx={{ color: '#4D69FF', fontSize: '1.2em', display: 'inline', fontFamily: 'RammettoOne' }}> . </Typography>
+            <Typography sx={{ color: '#1B1B1B', fontSize: { sm: '1.1em', md: '1.1em', lg: '1.4em' }, fontFamily: 'RammettoOne', lineHeight: '1.2em' }}>
+              in the world
+              <Typography variant={"div"} sx={{ color: '#4D69FF', fontSize: { sm: '1.5em', md: '1.5em', lg: '1.5em' }, display: 'inline', fontFamily: 'RammettoOne' }}> . </Typography>
             </Typography>
           </Grid>
-          <Grid item xs={1} />
+          <Grid item md={1} />
         </Grid>
         <Box height={250} style={{ backgroundColor: '#E9E6E2' }} />
         <ArrowIcon />
       </div>
       <Box height={150} />
-      
+
       <Grid container sx={{ display: 'flex', flexDirection: 'row' }}>
-        <Grid item xs={1} />
-        <Grid item xs={10}>
+        <Grid item md={1} />
+        <Grid item md={10}>
           <Typography sx={{ color: '#1B1B1B', fontSize: '1.125em', fontFamily: 'Jost-500', textAlign: 'center' }}>
             <Typography sx={{ color: '#4D69FF', fontSize: '1.6em', display: 'inline', fontFamily: 'RammettoOne' }}> . </Typography> &nbsp; What &nbsp; you &nbsp;  can &nbsp; expect &nbsp;
             <Typography sx={{ color: '#4D69FF', fontSize: '1.6em', display: 'inline', fontFamily: 'RammettoOne' }}> . </Typography>
           </Typography>
         </Grid>
-        <Grid item xs={1} />
+        <Grid item md={1} />
       </Grid>
 
-      <Grid container sx={{ display: 'flex', flexDirection: 'row', backgroundColor: '#E9E6E2', padding: '2% 0%', marginTop: '1%'}}>
-        <Grid item xs={1} />
-        <Grid item xs={10} >
-          <Typography sx={{ color: '#1B1B1B', fontSize: '1.5em', fontFamily: 'RammettoOne', width: '100%', textAlign: 'center'}}>
-          All about delivering top-notch work that  <br />
-          &nbsp; &nbsp; goes above and beyond expectations
+      <Grid container sx={{ display: 'flex', flexDirection: 'row', backgroundColor: '#E9E6E2', padding: '2% 0%', marginTop: '1%' }}>
+        <Grid item md={1} />
+        <Grid item md={10} >
+          <Typography sx={{ color: '#1B1B1B', fontSize: '1.5em', fontFamily: 'RammettoOne', width: '100%', textAlign: 'center' }}>
+            All about delivering
+            top-notch
+            work that  <br />
+            &nbsp; &nbsp; goes above and beyond expectations
           </Typography>
         </Grid>
-        <Grid item xs={1} />
+        <Grid item md={1} />
       </Grid>
 
       <Box height={60} />
-      
+
       <Grid container sx={{ display: 'flex', flexDirection: 'row' }}>
-        <Grid item xs={1} />
-        <Grid item xs={10}>
+        <Grid item md={1} />
+        <Grid item md={10}>
           <Typography sx={{ color: '#1B1B1B', fontSize: '1.125em', fontFamily: 'Jost-500', textAlign: 'center' }}>
             <Typography sx={{ color: '#4D69FF', fontSize: '1.6em', display: 'inline', fontFamily: 'RammettoOne' }}> . </Typography> &nbsp; Where &nbsp; we &nbsp; can &nbsp; help &nbsp; you &nbsp;
             <Typography sx={{ color: '#4D69FF', fontSize: '1.6em', display: 'inline', fontFamily: 'RammettoOne' }}> . </Typography>
           </Typography>
         </Grid>
-        <Grid item xs={1} />
+        <Grid item md={1} />
       </Grid>
 
 
