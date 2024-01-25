85cb97bf6d4c3879a886493d804e0af761549e56
ankit3146 <an225884@dal.ca>
2024-01-25 12:35:42 -0500

    Add xs to 600

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 const theme = createTheme({
   breakpoints: {
     values: {
-      xs: 0,
-      sm: 0,
+      xs: 600,
+      sm: 600,
       md: 960,
       lg: 1280,
       xl: 1920,
